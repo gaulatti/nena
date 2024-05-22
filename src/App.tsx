c18972cb9dc558dfac06e2794c6a0a6de0cdd6aa
Javier Godoy <rotvulpix@gmail.com>
2024-05-22 16:04:46 -0400

    feat: Hello World, again.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <iframe
+        width='560'
+        height='315'
+        src='https://www.youtube.com/embed/Fp2K-z-9nMQ?si=UKSfFg_Q02lsy7rl'
+        title='YouTube video player'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        referrerPolicy='strict-origin-when-cross-origin'
+        allowFullScreen
+      ></iframe>
+    </>
+  );
+}
+
+export default App;
