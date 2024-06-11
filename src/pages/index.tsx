e53e0ef6eda627bdcfa1fee5737e38d088443263
Javier Godoy <rotvulpix@gmail.com>
2024-06-11 02:28:03 -0400

    wip: Doing a few attempts on layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,34 @@
+import { Flex } from 'antd';
+
 const Home = () => {
   return (
-    <div>
-      <>
-        <iframe
-          width='560'
-          height='315'
-          src='https://www.youtube.com/embed/Fp2K-z-9nMQ?si=UKSfFg_Q02lsy7rl'
-          title='YouTube video player'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          referrerPolicy='strict-origin-when-cross-origin'
-          allowFullScreen
-        ></iframe>
-      </>
-    </div>
+    <>
+      <header>
+        <Flex justify='center'>
+          <section>
+            <Flex justify='space-between'>
+              <div>logo</div>
+              <div>logo</div>
+            </Flex>
+          </section>
+        </Flex>
+      </header>
+      <main>
+        <Flex justify='center'>
+          <section>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/Fp2K-z-9nMQ?si=UKSfFg_Q02lsy7rl'
+              title='YouTube video player'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              referrerPolicy='strict-origin-when-cross-origin'
+              allowFullScreen
+            ></iframe>
+          </section>
+        </Flex>
+      </main>
+    </>
   );
 };
 
